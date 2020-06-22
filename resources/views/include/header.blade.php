3b926f5d4cc638f6efb90150397c4144b0b92ed7
Ali Ibrahim <allosheribraheem38@gmail.com>
2020-06-23 01:05:33 +0300

    small mistake

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -91,7 +91,7 @@
 				<!-- Aside Nav -->
 				<div id="nav-aside">
 					<ul class="nav-aside-menu">
-						<li><a href="index.html">Home</a></li>
+						<li><a href="/">Home</a></li>
 						<li class="has-dropdown"><a>Categories</a>
 							<ul class="dropdown">
 									@foreach ($categories as $category)
@@ -128,8 +128,8 @@
                                 </ul>
                             </li>
                         @endguest
-						<li><a href="about.html">About Us</a></li>
-						<li><a href="contact.html">Contacts</a></li>
+						<li><a href="/about">About Us</a></li>
+						<li><a href="/contact">Contacts</a></li>
 						<li><a href="#">Advertise</a></li>
 					</ul>
 					<button class="nav-close nav-aside-close"><span></span></button>
